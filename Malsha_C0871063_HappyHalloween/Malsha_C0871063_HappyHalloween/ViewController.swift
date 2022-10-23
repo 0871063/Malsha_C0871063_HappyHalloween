36af86b36dece56eadf6628c49a361d807d8d82d
0871063 <c0871063@mylambton.ca>
2022-10-23 18:22:47 -0400

    Connect IBOutlet

--- Malsha_C0871063_HappyHalloween/Malsha_C0871063_HappyHalloween/ViewController.swift
+++ Malsha_C0871063_HappyHalloween/Malsha_C0871063_HappyHalloween/ViewController.swift
@@ -9,11 +9,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var iconImage: UIImageView!
+    @IBOutlet weak var livesLbl: UILabel!
+    @IBOutlet weak var pointsLbl: UILabel!
+    @IBOutlet weak var startBtn: UIButton!
+    @IBOutlet weak var answerFourBtn: UIButton!
+    @IBOutlet weak var answerThreeBtn: UIButton!
+    @IBOutlet weak var answerTwoBtn: UIButton!
+    @IBOutlet weak var answerOneBtn: UIButton!
+    
+    var pointCount = 0
+    var livesCount = 10
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    private func refreshData(){
+        livesCount = 10
+        pointCount = 0
+        iconImage.image = UIImage(named: "QuestionMark")
+        answerOneBtn.setTitle("???", for: .normal)
+        answerTwoBtn.setTitle("???", for: .normal)
+        answerThreeBtn.setTitle("???", for: .normal)
+        answerFourBtn.setTitle("???", for: .normal)
+        startBtn.isHidden = false
+        livesLbl.text = "Lives : " + String(livesCount)
+        pointsLbl.text = "Points : " + String(pointCount)
+    }
+    
+    @IBAction func startGame(_ sender: Any) {
+    }
+    
+    @IBAction func answerOneClicked() {
+    }
+    @IBAction func answerTwoClicked() {
+    }
+    @IBAction func answerThreeClicked() {
+    }
+    @IBAction func answerFourClicked() {
+    }
+    
 }
 
